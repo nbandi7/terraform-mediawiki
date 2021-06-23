706d8302a9dc61ac6888dc5106f1c39f6ca58d91
Nithin C Bandi <nithincbandi@gmail.com>
2021-06-24 03:57:28 +0530

    Update ec2.tf

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "ec2wiki" {
   subnet_id              = aws_subnet.publicSubnet[count.index].id
   vpc_security_group_ids = [aws_security_group.sg.id]
   user_data              = file("modules/user_data.sh")
-  key_name               = "test"
 
   tags = {
     Name  = "mediawiki-${count.index + 1}"
